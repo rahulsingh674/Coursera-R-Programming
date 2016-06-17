55ddbdac21e57d245006df1ccddf48aba5433576
rahulsingh674 <rahuliitd06@gmail.com>
2016-06-17 09:20:22 +0530

    Update CacheMatrix.R

--- CacheMatrix.R
+++ CacheMatrix.R
@@ -1,3 +1,9 @@
+# function to create cache matrix
+# total four functions are created
+# set the vector
+# get the vector
+# set the Inverse
+# get Inverse 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -13,7 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-
+# create Cache Solve which gives the inverse value stored in get$Inverse, if not prsesent it calculates the inverse matrix
 cacheSolve <- function(x, ...) {
   i <- x$getInverse()
   if (!is.null(i)) {
